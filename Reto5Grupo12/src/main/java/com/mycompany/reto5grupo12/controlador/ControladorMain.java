c52b16c643b65c8272edfea32bba1d8db90f8c1f
andrespaezs <111217584+andrespaezs@users.noreply.github.com>
2022-08-14 01:33:13 -0500

    Add files via upload

---
+++ Reto5Grupo12/src/main/java/com/mycompany/reto5grupo12/controlador/ControladorMain.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.reto5grupo12.controlador;
+
+import com.mycompany.reto5grupo12.vista.frmPrincipal;
+
+
+/**
+ *
+ * @author andre
+ */
+public class ControladorMain {
+    public static void main(String[] args){
+       
+        //Conexion cc = new Conexion ();
+        //Connection cn = cc.conectar();
+      
+       new frmPrincipal().setVisible(true);
+       
+       
+    }
+}
